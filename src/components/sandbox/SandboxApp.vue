14dbee638949c4b0e715927eabc3d10b83bd2216
Simon Kahl <simon@rds.com.au>
2019-03-06 23:50:42 +1030

    add route params

--- src/components/sandbox/SandboxApp.vue
+++ src/components/sandbox/SandboxApp.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="sandbox">
     <!--
-      passing props to component: pass kickers as kickers
+      Can receive params from the router ... see router/index.js for where this is passed.
+      They can also become props, with attendant validation and so on, as long as the route (in router/index.js)
+      defines   props: true   and this component defines to receive them below.
+      and they are
+    -->
+    <p>Route params: {{$route.params.param1}} ... also appearing as props: {{param1}}</p>
+    <!--
+      passing props to child component: pass kickers as kickers
       listening for events from component: listen for addPlayer and call addPlayersToKickers
     -->
     <sandbox-multi-player-list v-bind:kickers="kickers" v-on:addPlayer="addPlayerToKickers($event)"/>
@@ -14,6 +21,14 @@
   export default {
     components: {
       'sandbox-multi-player-list': SandboxMultiPlayerList
+    },
+
+    // receive route params as props (see above)
+    props: {
+      'param1': {
+        required: true,
+        type: String
+      }
     },
 
     data () {
