dc6a7e7b2f3fbf74f0600c201027c1660d062d11
Simon Kahl <simon@rds.com.au>
2019-03-11 19:34:54 +1030

    put PlayerList data in html table

--- src/components/drafting/PlayerList.vue
+++ src/components/drafting/PlayerList.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="player-list">
     <h1>{{ title }}</h1>
-    <ul>
-      <li v-for="p in players" :key="p.id" class="player" @click="selectPlayer(p)">
-        <span class="name" v-bind:class="{selected: selectedPlayer === p}">{{p.name}}</span>
-        <span class="aflClub">({{p.aflClub.substring(0, 4)}})</span>
-        <span class="stats">
-          <span class="k" v-bind:class="{highlight: highlightPosition == 'k'}">{{p.kicks / 10}}</span>
-          <span class="h" v-bind:class="{highlight: highlightPosition == 'h'}">{{p.handballs / 10}}</span>
-          <span class="m" v-bind:class="{highlight: highlightPosition == 'm'}">{{p.marks / 10}}</span>
-          <span class="r" v-bind:class="{highlight: highlightPosition == 'r'}">{{p.hitouts / 10}}</span>
-          <span class="t" v-bind:class="{highlight: highlightPosition == 't'}">{{p.tackles / 10}}</span>
-          <span class="g" v-bind:class="{highlight: highlightPosition == 'g'}">{{p.goals / 10}}</span>
-          <span class="s" v-bind:class="{highlight: highlightPosition == 's'}">{{p.star / 10}}</span>
-          <span class="games">{{p.played}}</span>
-        </span>
-      </li>
-    </ul>
+    <table>
+      <tbody>
+      <tr class="player"
+          v-for="p in players"
+          :id="highlightPosition + '--' + p.id"
+          :key="p.id"
+          @click="selectPlayer(p)">
+        <td class="name" :class="{selected: selectedPlayer === p}">{{p.name.substring(0, 20)}}</td>
+        <td class="aflClub">({{p.aflClub.substring(0, 4)}})</td>
+        <td class="k" :class="{highlight: highlightPosition == 'k'}">{{p.kicks / 10}}</td>
+        <td class="h" :class="{highlight: highlightPosition == 'h'}">{{p.handballs / 10}}</td>
+        <td class="m" :class="{highlight: highlightPosition == 'm'}">{{p.marks / 10}}</td>
+        <td class="r" :class="{highlight: highlightPosition == 'r'}">{{p.hitouts / 10}}</td>
+        <td class="t" :class="{highlight: highlightPosition == 't'}">{{p.tackles / 10}}</td>
+        <td class="g" :class="{highlight: highlightPosition == 'g'}">{{p.goals / 10}}</td>
+        <td class="s" :class="{highlight: highlightPosition == 's'}">{{p.star / 10}}</td>
+        <td class="games">{{p.played}}</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -27,11 +31,11 @@
     props: {
       title: {
         required: true,
-        type: Object
+        type: String
       },
       highlightPosition: {
         required: true,
-        type: Object
+        type: String
       },
       players: {
         required: true,
@@ -64,34 +68,20 @@
     margin: 0;
   }
 
-  .player-list ul {
-    list-style-type: none;
-    margin: 0px;
-    padding: 0px;
+  .player-list tbody {
+    display: block;
     width: 300px;
     height: 330px;
     overflow: scroll;
   }
 
-  .name {
-    font-size: 0.7em;
-  }
-
-  .aflClub {
+  .player td {
     font-size: 0.6em;
-  }
-
-  .stats {
-    font-size: 0.5em;
   }
 
   .highlight {
     font-weight: bold;
     font-size: 1.2em;
-  }
-
-  .games {
-    font-weight: bold;
   }
 
   .selected {
