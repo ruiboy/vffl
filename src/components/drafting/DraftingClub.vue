5b56f7be6cd9e0381ce494fed249e933b46f0107
Simon Kahl <simon@rds.com.au>
2019-03-14 21:58:38 +1030

    Add clubs to Drafting component

---
+++ src/components/drafting/DraftingClub.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="club">
+    <h1>{{ name }}</h1>
+    <ul>
+      <li>Players Drafted: {{ playerCount }}</li>
+      <li>Paid: {{ totalPaid }}</li>
+      <li>To Get: {{ playerCountRemaining }}</li>
+      <li>To Spend: {{ totalRemaining }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      name: {
+        required: false,
+        type: String
+      },
+      players: {
+        required: true,
+        type: Array
+      }
+    },
+
+    computed: {
+      playerCount () {
+        return this.players.length;
+      },
+
+      playerCountRemaining () {
+        return 30 - this.players.length;
+      },
+
+      totalPaid () {
+        return this.players.reduce((total, p) => total + p.cents, 0) / 100
+      },
+
+      totalRemaining () {
+        return this.players.reduce((total, p) => total - p.cents, 3500) / 100
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .club {
+    border: thin black solid;
+    font-size: 0.8em;
+    width: 170px;
+    height: 116px;
+  }
+
+  .club h1 {
+    font-size: 1.4em;
+  }
+</style>
