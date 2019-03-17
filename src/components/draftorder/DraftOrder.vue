6adfa1a4e2e927390cc7541aa27d634f80174416
Simon Kahl <simon@rds.com.au>
2019-03-17 21:05:35 +1030

    add chart.js - chart of players per afl club

--- src/components/draftorder/DraftOrder.vue
+++ src/components/draftorder/DraftOrder.vue
@@ -1,44 +1,35 @@
 <template>
   <div class="player-list">
-    <table>
-      <tbody>
-      <tr class="player"
-          v-for="p in players"
-          :id="p.id"
-          :key="p.id">
-        <td class="name">{{p.name}}</td>
-        <td class="aflClub" :title="p.aflClub">{{p.aflClub}}</td>
-        <td class="drafted-by">{{p.draftedBy}}</td>
-        <td class="paid">{{p.cents / 100}}</td>
-        <td class="drafted-ts">{{new Date(p.draftedTs).toLocaleString()}}</td>
-      </tr>
-      </tbody>
-    </table>
-    <div class="footer">
-      Adel {{getAflClubPlayerCount('Adelaide')}} &nbsp; &nbsp;
-      Bris {{getAflClubPlayerCount('Brisbane Lions')}} &nbsp; &nbsp;
-      Carl {{getAflClubPlayerCount('Carlton')}} &nbsp; &nbsp;
-      Coll {{getAflClubPlayerCount('Collingwood')}} &nbsp; &nbsp;
-      Ess {{getAflClubPlayerCount('Essendon')}} &nbsp; &nbsp;
-      Fre {{getAflClubPlayerCount('Fremantle')}} &nbsp; &nbsp;
-      Geel {{getAflClubPlayerCount('Geelong')}} &nbsp; &nbsp;
-      GCS {{getAflClubPlayerCount('Gold Coast Suns')}} &nbsp; &nbsp;
-      GWS {{getAflClubPlayerCount('Greater Western Sydney')}} &nbsp; &nbsp;
-      Haw {{getAflClubPlayerCount('Hawthorn')}} &nbsp; &nbsp;
-      NM {{getAflClubPlayerCount('Kangaroos')}} &nbsp; &nbsp;
-      Melb {{getAflClubPlayerCount('Melbourne')}} &nbsp; &nbsp;
-      Port {{getAflClubPlayerCount('Port Adelaide')}} &nbsp; &nbsp;
-      Rich {{getAflClubPlayerCount('Richmond')}} &nbsp; &nbsp;
-      SK {{getAflClubPlayerCount('St Kilda')}} &nbsp; &nbsp;
-      Syd {{getAflClubPlayerCount('Sydney Swans')}} &nbsp; &nbsp;
-      WCE {{getAflClubPlayerCount('West Coast Eagles')}} &nbsp; &nbsp;
-      WB {{getAflClubPlayerCount('Western Bulldogs')}}
+    <div class="players">
+      <table>
+        <tbody>
+        <tr class="player"
+            v-for="p in players"
+            :id="p.id"
+            :key="p.id">
+          <td class="name">{{p.name}}</td>
+          <td class="aflClub" :title="p.aflClub">{{p.aflClub}}</td>
+          <td class="drafted-by">{{p.draftedBy}}</td>
+          <td class="paid">{{p.cents / 100}}</td>
+          <td class="drafted-ts">{{new Date(p.draftedTs).toLocaleString()}}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="chart">
+      <chart-players-per-afl-club></chart-players-per-afl-club>
     </div>
   </div>
 </template>
 
 <script>
+  import ChartPlayersPerAflClub from './ChartPlayersPerAflClub.vue'
+
   export default {
+    components: {
+      'chart-players-per-afl-club': ChartPlayersPerAflClub
+    },
+
     computed: {
       players () {
         return this.$store.getters.getDraftedPlayers
@@ -47,7 +38,7 @@
 
     methods: {
       getAflClubPlayerCount (aflClub) {
-        return this.players.filter(p => p.aflClub == aflClub).length
+        return this.players.filter(p => p.aflClub === aflClub).length
       }
     }
   }
@@ -58,20 +49,21 @@
     border: thin gray solid;
   }
 
+  .player-list div {
+    float: left;
+  }
+
   .player-list tbody {
     // make only the table body scroll within a view port
     display: block;
     height: 694px;
+    font-size: 0.8em;
     overflow: scroll;
   }
 
   // put a line every 6th row
   .player-list tr:nth-child(6n+0) td {
     border-bottom: thin gray solid;
-  }
-
-  .footer {
-    font-size: 0.8em;;
   }
 
   // three selectors below add row numbers at start of each table row
